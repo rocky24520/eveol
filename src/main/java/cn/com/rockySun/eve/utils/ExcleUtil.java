05afe2b607ba18a8fe778a416c92e9d2196e12bb
rockySun <599125734@qq.com>
2018-12-14 16:18:16 +0800

    1

--- src/main/java/cn/com/rockySun/eve/utils/ExcleUtil.java
+++ src/main/java/cn/com/rockySun/eve/utils/ExcleUtil.java
@@ -12,6 +12,8 @@ import java.nio.file.FileSystemNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.util.StringUtils;
+
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -19,9 +21,120 @@ import jxl.read.biff.BiffException;
 public class ExcleUtil {
 
 	public static void main(String[] args) {
-//		aaa();
-		bbb();
+		yewuyuSql();
+	}
+	
+	/**
+	 * 根据表规则文档生成表规则脚本
+	 * @Description	: 
+	 * @Author		: 
+	 * @CreateTime	: 2018年12月14日 下午4:06:55
+	 */
+	public static void tableRuleSql(){
+		
+	}
+	
+	/**
+	 * 根据业务域对照文档生成业务域脚本
+	 * @Description	: 
+	 * @Author		: 
+	 * @CreateTime	: 2018年12月14日 下午3:25:31
+	 */
+	public static void yewuyuSql(){
+		File file = new File("D:/tmp/20181214002.xls");
+		File file1 = new File("D:/tmp/20181214002.sql");
+		int index = 0;
+		List<List<String>> excelList = readExcel(file);
+		try {
+			if(!file1.exists()){
+				file1.createNewFile();
+			}
+	        FileOutputStream out=new FileOutputStream(file1,false);
+	        StringBuffer stringBuffer = new StringBuffer();
+	        stringBuffer.append("DELETE FROM QC_BPID_PUB_TABLE_INFOR;");
+			stringBuffer.append("\r\n");
+	        for(List<String> list : excelList){
+				if (index < 2){
+					index ++;
+					continue;
+				}
+				if(StringUtils.isEmpty(list.get(0))){
+					break;
+				}
+				StringBuffer insert = new StringBuffer();
+				insert.append("INSERT INTO QC_BPID_PUB_TABLE_INFOR(ID,CLASS_CODE,CLASS_NAME,BASIC_ACTIVE_CODE"
+								+ ",BASIC_ACTIVE_NAME,TABLE_CODE,TABLE_NAME,USED_FLAG，MASTER_SLAVE，DEFINED2) values (");
 
+				insert.append("'"+java.util.UUID.randomUUID().toString().substring(0, 31)+"',");
+				insert.append("'"+list.get(0)+"',");
+				insert.append("'"+list.get(1)+"',");
+				insert.append("'"+list.get(2)+"',");
+				insert.append("'"+list.get(3)+"',");
+				insert.append("'"+list.get(4)+"',");
+				insert.append("'"+list.get(5)+"',");
+				insert.append("'"+list.get(6)+"',");
+				insert.append("'"+list.get(7)+"',");
+				insert.append("'"+list.get(9)+"'");
+				insert.append(");");
+				stringBuffer.append(insert.toString()+"\r\n");
+				index ++;
+			}
+	        stringBuffer.append("COMMIT;");
+			out.write(stringBuffer.toString().getBytes("utf-8"));
+			out.flush();
+			out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	/**
+	 * 根据文档生成值域对照表脚本
+	 * @Description	: 
+	 * @Author		: 
+	 * @CreateTime	: 2018年12月14日 下午3:20:48
+	 */
+	public static void rangeCompareSql(){
+		File file = new File("D:/tmp/20181214001.xls");
+		File file1 = new File("D:/tmp/20181214001.sql");
+		int index = 0;
+		List<List<String>> excelList = readExcel(file);
+		try {
+			if(!file1.exists()){
+				file1.createNewFile();
+			}
+	        FileOutputStream out=new FileOutputStream(file1,false);
+	        StringBuffer stringBuffer = new StringBuffer();
+	        stringBuffer.append("DELETE FROM RANGE_COMPARE;");
+			stringBuffer.append("\r\n");
+	        for(List<String> list : excelList){
+				if (index < 2){
+					index ++;
+					continue;
+				}
+				StringBuffer insert = new StringBuffer();
+				insert.append("INSERT INTO RANGE_COMPARE(LV_ONE_CODE,LV_ONE_NAME,LV_ONE_VALUE,LV_ONE_COMMENT"
+								+ ",LV_TWO_CODE,LV_TWO_NAME,LV_TWO_VALUE,LV_TWO_COMMENT) values (");
+				insert.append("'"+list.get(0)+"',");
+				insert.append("'"+list.get(1)+"',");
+				insert.append("'"+list.get(2)+"',");
+				insert.append("'"+list.get(3)+"',");
+				insert.append("'"+list.get(5)+"',");
+				insert.append("'"+list.get(6)+"',");
+				insert.append("'"+list.get(7)+"',");
+				insert.append("'"+list.get(8)+"'");
+				insert.append(");");
+				stringBuffer.append(insert.toString()+"\r\n");
+				index ++;
+			}
+	        stringBuffer.append("COMMIT;");
+			out.write(stringBuffer.toString().getBytes("utf-8"));
+			out.flush();
+			out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
 	}
 	public static void aaa(){
 		File file = new File("D:/tmp/2222.xls");
